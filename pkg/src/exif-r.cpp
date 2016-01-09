31b3857ce73d5a3dfd8596cad766ba9139e435d0
Henning Redestig <henning.red@gmail.com>
2016-01-09 12:50:05 +0100

    ..

--- pkg/src/exif-r.cpp
+++ pkg/src/exif-r.cpp
@@ -8,39 +8,39 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 CharacterVector date_original(SEXP file) {
   try{
-	Rcpp::CharacterVector cfile(file);
-	Rcpp::CharacterVector res(cfile.size());
+    Rcpp::CharacterVector cfile(file);
+    Rcpp::CharacterVector res(cfile.size());
 
-	for(int i = 0; i < cfile.size(); i++) {
-	  FILE *fp = fopen(cfile(i), "rb");
-	  if(!fp) { 
-		throw std::invalid_argument("Can't open file.\n"); 
-	  }
-	  fseek(fp, 0, SEEK_END);
-	  unsigned long fsize = ftell(fp);
-	  rewind(fp);
-	  unsigned char *buf = new unsigned char[fsize];
-	  if(fread(buf, 1, fsize, fp) != fsize) {
-		throw std::invalid_argument("Can't read file.\n"); 
-	  }
-	  fclose(fp);
-	  // Parse exif
-	  try{
-		if(fsize > 0.1) {
-		  EXIFInfo result;
-		  ParseEXIF(buf, fsize, result);
-		  res(i) = result.dateTimeOriginal;
-		} else {
-		  res(i) = "NA";
-		}
-	  } catch (...){
-		return R_NilValue;
-	  }
+    for(int i = 0; i < cfile.size(); i++) {
+      FILE *fp = fopen(cfile(i), "rb");
+      if(!fp) { 
+	throw std::invalid_argument("Can't open file.\n"); 
+      }
+      fseek(fp, 0, SEEK_END);
+      unsigned long fsize = ftell(fp);
+      rewind(fp);
+      unsigned char *buf = new unsigned char[fsize];
+      if(fread(buf, 1, fsize, fp) != fsize) {
+	throw std::invalid_argument("Can't read file.\n"); 
+      }
+      fclose(fp);
+      // Parse exif
+      try{
+	if(fsize > 0.1) {
+	  EXIFInfo result;
+	  ParseEXIF(buf, fsize, result);
+	  res(i) = result.dateTimeOriginal;
+	} else {
+	  res(i) = "NA";
 	}
-	return res;
+      } catch (...){
+	return R_NilValue;
+      }
+    }
+    return res;
 
-  }catch(...) {
-	::Rf_error("unknown error in date_original");
+  } catch(...) {
+    ::Rf_error("unknown error in date_original");
   }
   return(0);
 }
